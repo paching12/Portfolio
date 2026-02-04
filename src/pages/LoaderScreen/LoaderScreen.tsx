bb81d5e3a8f7e4c7b344a0d1dbebbf60f7759e18
José Pach <Paching12@icloud.com>
2026-02-03 20:38:54 -0600

    Loading screen

--- src/pages/LoaderScreen/LoaderScreen.tsx
+++ src/pages/LoaderScreen/LoaderScreen.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import style from "./LoaderScreen.module.css";
 import { Loader } from "@components/atoms/Loader";
 
 const LoaderScreen = () => {
   return (
     <div className={style.mainScreen}>
-      <Loader />
       <div className={style.techPattern}></div>
+      <Loader />
     </div>
   );
 };
