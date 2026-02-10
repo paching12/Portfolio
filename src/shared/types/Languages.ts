0a1628508a122480a0d1155b446d250d0bd0c73f
José Pach <Paching12@icloud.com>
2026-02-10 11:10:22 -0600

    update valid languages

--- src/shared/types/Languages.ts
+++ src/shared/types/Languages.ts
@@ -1,15 +1,17 @@
 import { languageNames } from "@utils/locales/settings";
 
 export const LANGUAGES = {
-  AF: languageNames.afrikaans,
-  EN_US: languageNames.english,
-  EN_AU: languageNames.englishAu,
-  EN_GB: languageNames.englishGB,
-  EN_NZ: languageNames.englishNZ,
-  ES_MX: languageNames.espanolMx,
-  ES: languageNames.espanol,
-  PT_BR: languageNames.portugese,
-  EN: languageNames.english, // alias
+  // AF: languageNames.afrikaans,
+  // EN_US: languageNames.english,
+  // EN_AU: languageNames.englishAu,
+  // EN_GB: languageNames.englishGB,
+  // EN_NZ: languageNames.englishNZ,
+  // ES_MX: languageNames.espanolMx,
+  en: languageNames.english,
+  es: languageNames.espanol,
+  // pt: languageNames.portugese,
+  // PT_BR: languageNames.portugese,
+  // EN: languageNames.english, // alias
 } as const;
 
 export type LANGUAGE_TYPES = (typeof LANGUAGES)[keyof typeof LANGUAGES];
--- src/utils/locales/settings.ts
+++ src/utils/locales/settings.ts
@@ -1,12 +1,11 @@
 export const languageNames = {
-  afrikaans: "af",
-  english: "en-us",
-  englishAu: "en-au",
-  englishGB: "en-gb",
-  englishNZ: "en-nz",
-  espanolMx: "es-mx",
+  english: "en",
+  // englishAu: "en-au",
+  // englishGB: "en-gb",
+  // englishNZ: "en-nz",
+  // espanolMx: "es-mx",
   espanol: "es",
-  portugese: "pt-br",
+  // portugese: "pt",
 } as const;
 
 export type LANGUAGES = (typeof languageNames)[keyof typeof languageNames];
