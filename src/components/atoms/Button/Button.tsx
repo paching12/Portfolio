1a8e90faa6b8a371644c1e3e936a7476ed363f69
José Pach <Paching12@icloud.com>
2026-02-11 20:06:12 -0600

    Improve button to received ref prop

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,18 +1,29 @@
-import { type FC } from "react";
+import { forwardRef } from "react";
 import { BUTTON_STYLES, type ButtonProps } from "./Button.types";
 import "./Button.css";
 
-const Button: FC<ButtonProps> = ({
-  children,
-  onClick,
-  style = "cursor-pointer",
-  variant = BUTTON_STYLES.PRIMARY,
-}) => {
-  return (
-    <button onClick={onClick} className={`btn btn-${variant} ${style}`}>
-      {children}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      onClick,
+      style = "cursor-pointer",
+      variant = BUTTON_STYLES.PRIMARY,
+    },
+    ref,
+  ) => {
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        className={`btn btn-${variant} ${style}`}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Button.displayName = "Button";
 
 export default Button;
