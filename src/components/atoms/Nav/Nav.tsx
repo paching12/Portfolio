bbd3fc523506fb6cfde19165386140bf4539f8d0
José Pach <Paching12@icloud.com>
2026-02-12 06:54:08 -0600

    update new path for language selector component

--- src/components/atoms/Nav/Nav.tsx
+++ src/components/atoms/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 import type { NavProps } from "./Nav.types";
 import { memo, type FC } from "react";
-import { LanguageSelector } from "../LanguageSelector";
+import { LanguageSelector } from "../../molecules/LanguageSelector";
 import MenuItemsList from "@components/molecules/MenuItemList/MenuItemList";
 
 const Nav: FC<NavProps> = ({ items }) => {
