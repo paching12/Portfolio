b7b00670e1014cadef3c6151361ec33b0b54603c
José Pach <Paching12@icloud.com>
2026-02-14 17:19:39 -0600

    fix typo

--- src/utils/locales/settings.ts
+++ src/utils/locales/settings.ts
@@ -1,7 +1,7 @@
 export const languageNames = {
   english: "en-us",
   espanol: "es-mx",
-  portugese: "pt-br",
+  portuguese: "pt-br",
 } as const;
 
 export type LANGUAGES = (typeof languageNames)[keyof typeof languageNames];
