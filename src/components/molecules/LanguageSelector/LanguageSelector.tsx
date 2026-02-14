9c647963e318ad8318f95245843bbc05acc54174
José Pach <Paching12@icloud.com>
2026-02-14 10:05:34 -0600

    implemented flags

--- src/components/molecules/LanguageSelector/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector/LanguageSelector.tsx
@@ -3,6 +3,8 @@ import type { LANGUAGE_TYPES } from "@shared/types/Languages";
 import { useCallback } from "react";
 import "./LanguageSelector.css";
 import Dropdown from "@components/molecules/Dropdown/Dropdown";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import classNames from "classnames";
 
 const LanguageSelector = () => {
   const {
@@ -21,10 +23,35 @@ const LanguageSelector = () => {
   return (
     <li className="language-container">
       <Dropdown
-        options={availableLanguages}
-        value={currentLanguage}
+        value={currentLanguage as LANGUAGE_TYPES}
         onChange={selectLanguageHandler}
         rounded
+        options={availableLanguages}
+        style="w-20"
+        renderItems={(item: LANGUAGE_TYPES, index = 0) => {
+          const [language, location] = item.split("-");
+          return (
+            <div
+              className={classNames(
+                {
+                  "dropdown-line": index,
+                },
+                "flex w-full justify-between py-0.5",
+              )}
+            >
+              <div className="flex px-2 py-1.5">
+                <span
+                  className={`fi fi-${location} fis flag-circle rounded-full`}
+                ></span>
+              </div>
+              <div className="flex flex-1 content-center items-center justify-center">
+                <span className="language-label">
+                  {language.toLocaleUpperCase()}
+                </span>
+              </div>
+            </div>
+          );
+        }}
       />
     </li>
   );
