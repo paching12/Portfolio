5833f1c1cf641191859f25d2a41180fde7bff18b
José Pach <Paching12@icloud.com>
2026-02-16 12:36:48 -0600

    Remove logo from Header

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,11 +1,9 @@
 import type { FC } from "react";
-import Logo from "@assets/JJPD_optimized.svg";
 import type { HeaderProps } from "./Header.types";
 
 const Header: FC<HeaderProps> = ({ children }) => {
   return (
-    <header className="m-6.25 flex justify-between">
-      <img src={Logo} alt="Your SVG" className="w-20 md:w-27 lg:w-37" />
+    <header className="m-6.25 flex justify-end">
       {/* Menu */}
       {children}
     </header>
